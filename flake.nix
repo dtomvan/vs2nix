c3d8e4308e924f2e4eb8227bf7ba1cc5c77578d8
Tom van Dijk <18gatenmaker6@gmail.com>
2025-07-06 00:21:07 +0200

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:numtide/nixpkgs-unfree/nixos-unstable";
+    nixpkgs.url = "github:numtide/nixpkgs-unfree";
+    nixpkgs.inputs.nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
     import-tree.url = "github:vic/import-tree";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
