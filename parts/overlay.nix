a6aea92948fc24b5a5ee274e1d1456e7eec8d5ec
Tom van Dijk <18gatenmaker6@gmail.com>
2025-09-14 12:26:04 +0200

    chore: get rid of overlay
    moving to NUR

--- parts/overlay.nix
+++ parts/overlay.nix
@@ -7,8 +7,6 @@ let
       doOverlay = builtins.hasAttr system self.legacyPackages;
       overlayContents =
         self.legacyPackages.${system}
-        # vintagestory is unfree, need to pass it like this so that `nixpkgs.config.allowUnfree` gets respected
-        // (import ../misc/vintagestory-overlay.nix { pkgs = prev; })
         // {
           inherit (self.packages.${system}) rustique;
         };
