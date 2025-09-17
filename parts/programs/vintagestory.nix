be186e1bbb72ac185cb9efe61750568a6bdf1658
Tom van Dijk <18gatenmaker6@gmail.com>
2025-09-17 11:50:08 +0200

    fix: remove superfluous parts/programs/vintagestory.nix

--- parts/programs/vintagestory.nix
+++
@@ -1,9 +0,0 @@
-# version to keep more up-to-date maybe (nixpkgs PRs aren't really addressed
-# yet by any committer)
-{
-  perSystem =
-    { pkgs, ... }:
-    {
-      packages = import ../../misc/vintagestory-overlay.nix { inherit pkgs; };
-    };
-}
